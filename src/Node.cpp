e9f69f0ae318d54c4ff597ffa3399d7e1dc2b4ca
MohamedA-M-Hassan <anehmr0@gmail.com>
2016-04-15 21:49:15 +0200

    endintation

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,9 +1,6 @@
 #include "Node.h"
-Node :: Node(Requirement r)
-{
-	require = r; next = nullptr;
-}
+Node::                  Node(Requirement r){require = r; next = nullptr;}
 void Node::             setData(Requirement r)   { require = r; }
-Requirement Node::        get_data()               { return require; }
+Requirement Node::      get_data()               { return require; }
 void Node::             set_next(node* n)        { next = r; }
 Node* Node::            get_next()               { return next; }
