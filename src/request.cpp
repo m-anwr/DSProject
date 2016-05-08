171ce82031d0da514fe9e5a7defd2d3fdfd58c4b
Shaza-Is <shaza993@yahoo.com>
2016-05-08 14:39:53 +0200

    Request added

--- src/request.cpp
+++ src/request.cpp
@@ -1,6 +1,30 @@
 #include "request.h"
 
-Request::Request()
+Request::Request(int time_, int from_, int to_)
 {
-
+    time = time_;
+    from = from_;
+    to = to_;
 }
+
+int Request::getTime()
+{
+    return time;
+}
+
+int Request::getFrom()
+{
+    return from;
+}
+
+int Request::getTo()
+{
+    return to;
+}
+
+bool Request::operator ==(Request &req)
+{
+    return ((this->getTime() == req.getTime()) && (this->getFrom() == req.getFrom()) && (this->getTo() == req.getTo()));
+}
+
+
--- src/request.h
+++ src/request.h
@@ -5,9 +5,18 @@
 class Request
 {
 public:
-    Request();
+    Request(int time_, int from_, int to_);
 
+    int getTime();
+    int getFrom();
+    int getTo();
+
+    bool operator ==(Request &req);
     //Operator== should be overloaded to check for equal requests and remove duplicates
+private:
+    int time;
+    int from;
+    int to;
 };
 
 #endif // REQUEST_H
